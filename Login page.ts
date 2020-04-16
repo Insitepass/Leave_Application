2a023db01037239a158faab8f1f8282790fb475e
Insitepass <38411152+Insitepass@users.noreply.github.com>
2020-04-16 14:06:55 +0200

    Create Login page.ts

---
+++ Login page.ts
@@ -0,0 +1,72 @@
+// this is the Ts page of the login.
+
+import { Component ,OnInit} from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import {AuthenticationServiceModule} from '../services/Firebase-Authentication.service';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
+})
+export class HomePage {
+  
+
+  validationform: FormGroup;
+  errormessage: string = "";
+  
+ 
+ 
+ constructor(private navCtrl:NavController,private authService:AuthenticationServiceModule, private formBuilder:FormBuilder) {}
+ 
+ngOnInit(){
+
+  this.validationform = this.formBuilder.group({
+    Email: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    ])),
+    Password: new FormControl('', Validators.compose([
+      Validators.minLength(8),
+      Validators.required
+    ])),
+  });
+}
+
+validation_messages = {
+  'Email': [
+    { type: 'required', message: 'Email is required.' },
+    { type: 'pattern', message: 'Please enter a valid email.' }
+  ],
+  'Password': [
+    { type: 'required', message: 'Password is required.' },
+    { type: 'minlength', message: 'Password must be at least 8 characters long.' }
+  ]
+};
+
+
+LoginUser(value){
+  this.authService.LoginUser(value)
+  .then(res => {
+    console.log(res);
+    this.errormessage = "";
+    this.navCtrl.navigateForward('dashboard');
+  }, err => {
+    this.errormessage = err.message;
+  })
+}
+
+RegisterPage(){
+  this.navCtrl.navigateForward('register');
+}
+ 
+ForgotPassword(){
+  this.navCtrl.navigateForward('password-reset');
+}
+  
+}
+ 
+
